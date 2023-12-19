893d3cb6a758300176b3a74af6809a16821455a5
JUSTIN PAGÈS <spyrojustin@gmail.com>
2023-12-19 12:22:56 -0500

    3 - Requête vers l'API

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { useState, useEffect } from "react";
+
+//Modules externes
+const axios = require("axios");
 
 //Components
 
@@ -6,7 +10,38 @@ import React from "react";
 import "../styles/gallery.css";
 
 export default function Gallery() {
-  const data = null;
+  const [data, setData] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("Cats");
+
+  useEffect(() => {
+    async function getPhotos(searchQuery) {
+      const unsplashApiKey = "VOTRE_CLE_D'ACCES_UNSLAPSH";
+
+      try {
+        const apiResponse = await axios.get(
+          "https://api.unsplash.com/search/photos",
+          {
+            params: {
+              query: searchQuery,
+              page: 1,
+              per_page: 30,
+            },
+            headers: {
+              Authorization: `Client-ID ${unsplashApiKey}`,
+            },
+          }
+        );
+
+        var photos = apiResponse.data.results;
+
+        setData({ photos: photos });
+      } catch (error) {
+        setData({ errors: [error.message] });
+      }
+    }
+
+    getPhotos(searchQuery);
+  }, [searchQuery]);
 
   return (
     <>
@@ -15,7 +50,7 @@ export default function Gallery() {
         <div id="loading">Loading...</div>
       ) : data.errors ? (
         <div id="error">
-          <small>Erreur !</small>
+          <small>{data.errors}</small>
         </div>
       ) : (
         <div id="gallery-container">
--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow, protocol } = require("electron");
+const path = require("path");
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
+if (require("electron-squirrel-startup")) {
   app.quit();
 }
+
+protocol.registerSchemesAsPrivileged([
+  //Pour autoriser la réception de données provenant de sites sécurisés
+  {
+    scheme: "https",
+    privileges: {
+      standard: true,
+      bypassCSP: true,
+      allowServiceWorkers: true,
+      supportFetchAPI: true,
+      corsEnabled: true,
+      stream: true,
+    },
+  },
+  //Pour autoriser l'envoie d'email
+  { scheme: "mailto", privileges: { standard: true } },
+]);
 
 const createWindow = () => {
   // Create the browser window.
@@ -27,18 +44,18 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
